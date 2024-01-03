7e5f3ad5c0dcc6626ee8dee4c43f22da70a1a1f7
Silvio Cesar <47402133+scs999@users.noreply.github.com>
2024-01-03 18:19:20 -0300

    Add files via upload

---
+++ Mapa_Local/google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-GwANoUOCefOw_Uljv7Ul66BLaU15sfRGTMjVYVoJFA/edit#gid=670446197';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBp2pdK3gE1KGZc_XxDdlQEVC3nUpAyuOM';
